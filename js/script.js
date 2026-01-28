d5818b39992ac1d5474b0df1c31044ee33c3073d
meow-meow <hananona_nanohana@hotmail.com>
2026-01-28 16:41:15 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+// モーダル
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const closeBtn = document.querySelector(".close");
+
+document.querySelectorAll(".card img").forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+  });
+});
+
+closeBtn.onclick = () => modal.style.display = "none";
+modal.onclick = e => {
+  if (e.target === modal) modal.style.display = "none";
+};
+// 戻るボタン
+document.getElementById('back-to-top').addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
