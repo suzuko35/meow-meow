7d5de2a8e34595111e09cc46b0eef7b318b6af67
meow-meow <hananona_nanohana@hotmail.com>
2026-02-09 23:24:30 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 
 // モーダル
-const modal = document.getElementById("modal");
-const modalImg = document.getElementById("modal-img");
-const closeBtn = document.querySelector(".close");
+// const modal = document.getElementById("modal");
+// const modalImg = document.getElementById("modal-img");
+// const closeBtn = document.querySelector(".close");
 
-document.querySelectorAll(".card img").forEach(img => {
-  img.addEventListener("click", () => {
-    modal.style.display = "flex";
-    modalImg.src = img.src;
-  });
-});
+// document.querySelectorAll(".card img").forEach(img => {
+//   img.addEventListener("click", () => {
+//     modal.style.display = "flex";
+//     modalImg.src = img.src;
+//   });
+// });
 
 closeBtn.onclick = () => modal.style.display = "none";
 modal.onclick = e => {
@@ -57,38 +57,20 @@ document.querySelectorAll(".carousel-item img").forEach(img => {
 //   });
 // });
 
-window.addEventListener("DOMContentLoaded", () => {
-  // モーダルを取得
-  const modal = document.getElementById("modal");
-  // モーダルを表示するボタンを全て取得
-  const openModalBtns = document.querySelectorAll(".js-open-modal");
-  // モーダルを閉じるボタンを全て取得
-  const closeModalBtns = document.querySelectorAll(".js-close-modal");
-
-  // Swiperの設定
-  const swiper = new Swiper(".swiper", {
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    spaceBetween: 30,
-  });
-
-  // モーダルを表示するボタンをクリックしたとき
-  openModalBtns.forEach((openModalBtn) => {
-    openModalBtn.addEventListener("click", () => {
-      // data-slide-indexに設定したスライド番号を取得
-      const modalIndex = openModalBtn.dataset.slideIndex;
-      swiper.slideTo(modalIndex);
-      modal.classList.add("is-active");
-    });
-  });
-
-  // モーダルを閉じるボタンをクリックしたとき
-  closeModalBtns.forEach((closeModalBtn) => {
-    closeModalBtn.addEventListener("click", () => {
-      modal.classList.remove("is-active");
-    });
-  });
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  // ページネーション
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // 前後の矢印
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // スクロールバー
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
 });
