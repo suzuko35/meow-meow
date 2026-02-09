99162062e8f609f4dd1785d754fba1de6633d70b
meow-meow <hananona_nanohana@hotmail.com>
2026-02-10 00:22:38 +0900

    update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 
 // モーダル
-// const modal = document.getElementById("modal");
-// const modalImg = document.getElementById("modal-img");
-// const closeBtn = document.querySelector(".close");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const closeBtn = document.querySelector(".close");
 
-// document.querySelectorAll(".card img").forEach(img => {
-//   img.addEventListener("click", () => {
-//     modal.style.display = "flex";
-//     modalImg.src = img.src;
-//   });
-// });
+document.querySelectorAll(".card img").forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+  });
+});
 
-// closeBtn.onclick = () => modal.style.display = "none";
-// modal.onclick = e => {
-//   if (e.target === modal) modal.style.display = "none";
-// };
+closeBtn.onclick = () => modal.style.display = "none";
+modal.onclick = e => {
+  if (e.target === modal) modal.style.display = "none";
+};
+
 // 戻るボタン
 document.getElementById('back-to-top').addEventListener('click', () => {
   window.scrollTo({
@@ -30,32 +31,6 @@ document.querySelectorAll(".carousel-item img").forEach(img => {
   });
 });
 
-
-
-// $(function(){
-//   $('.slider').slick({
-//     autoplay: true, //自動でスクロール
-//     autoplaySpeed: 0, //自動再生のスライド切り替えまでの時間を設定
-//     speed: 5000, //スライドが流れる速度を設定
-//     cssEase: "linear", //スライドの流れ方を等速に設定
-//     slidesToShow: 4, //表示するスライドの数
-//     swipe: true, // 操作による切り替え
-//     arrows: true, //矢印
-//     pauseOnFocus: true, //スライダーをフォーカスした時にスライドを停止させるか
-//     pauseOnHover: true, //スライダーにマウスホバーした時にスライドを停止させるか
-//     swipe: true,
-//     infinite: true,
-//     focusOnSelect	: true,
-//     responsive: [
-//       {
-//         breakpoint: 750,
-//         settings: {
-//           slidesToShow: 3, //画面幅750px以下でスライド3枚表示
-//         }
-//       }
-//     ]
-//   });
-// });
 
 const swiper = new Swiper(".swiper", {
   loop: true,
