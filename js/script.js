77342b80c1f08aeb896bc04f195fedcdfa4df71a
meow-meow <hananona_nanohana@hotmail.com>
2026-02-19 15:15:37 +0900

    update

--- js/script.js
+++ js/script.js
@@ -97,7 +97,7 @@ class Particle {
   constructor() {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 3;
+    this.size = Math.random() * 2;
     this.speedX = (Math.random() - 0.5) * 0.4;
     this.speedY = (Math.random() - 0.5) * 0.4;
   }
@@ -116,7 +116,7 @@ class Particle {
 }
 
 const particles = [];
-for (let i = 0; i < 600; i++) particles.push(new Particle());
+for (let i = 0; i < 1000; i++) particles.push(new Particle());
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
