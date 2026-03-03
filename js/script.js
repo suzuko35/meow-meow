792f44aba0fa559845755efeec9c15ae9631d514
meow-meow <hananona_nanohana@hotmail.com>
2026-03-03 13:48:54 +0900

    update

--- js/script.js
+++ js/script.js
@@ -82,49 +82,49 @@ $('.image').modaal({
 });
 
 
+//パーティクル背景
+// const canvas = document.getElementById("particleCanvas");
+// const ctx = canvas.getContext("2d");
 
-const canvas = document.getElementById("particleCanvas");
-const ctx = canvas.getContext("2d");
+// function resize() {
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+// }
+// resize();
+// window.addEventListener("resize", resize);
 
-function resize() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-resize();
-window.addEventListener("resize", resize);
+// class Particle {
+//   constructor() {
+//     this.x = Math.random() * canvas.width;
+//     this.y = Math.random() * canvas.height;
+//     this.size = Math.random() * 2;
+//     this.speedX = (Math.random() - 0.5) * 0.4;
+//     this.speedY = (Math.random() - 0.5) * 0.4;
+//   }
+//   update() {
+//     this.x += this.speedX;
+//     this.y += this.speedY;
+//     if (this.x < 0) this.x = canvas.width;
+//     if (this.x > canvas.width) this.x = 0;
+//     if (this.y < 0) this.y = canvas.height;
+//     if (this.y > canvas.height) this.y = 0;
+//   }
+//   draw() {
+//     ctx.fillStyle = "rgba(77, 77, 77, 0.46)";
+//     ctx.fillRect(this.x, this.y, this.size, this.size);
+//   }
+// }
 
-class Particle {
-  constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 2;
-    this.speedX = (Math.random() - 0.5) * 0.4;
-    this.speedY = (Math.random() - 0.5) * 0.4;
-  }
-  update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-    if (this.x < 0) this.x = canvas.width;
-    if (this.x > canvas.width) this.x = 0;
-    if (this.y < 0) this.y = canvas.height;
-    if (this.y > canvas.height) this.y = 0;
-  }
-  draw() {
-    ctx.fillStyle = "rgba(77, 77, 77, 0.46)";
-    ctx.fillRect(this.x, this.y, this.size, this.size);
-  }
-}
+// const particles = [];
+// for (let i = 0; i < 1600; i++) particles.push(new Particle());
 
-const particles = [];
-for (let i = 0; i < 1600; i++) particles.push(new Particle());
-
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  particles.forEach(p => {
-    p.update();
-    p.draw();
-  });
-  requestAnimationFrame(animate);
-}
-animate();
+// function animate() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   particles.forEach(p => {
+//     p.update();
+//     p.draw();
+//   });
+//   requestAnimationFrame(animate);
+// }
+// animate();
 
